70e28705abe8a8830961926c0446e84f8ee0ac18
not-zendaya <ukzendaya@gmail.com>
2025-12-23 15:11:02 +0300

    Added random dessert generator structure and functionality

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function generateRandomDessert() {
+    const random = desserts[Math.floor(Math.random() * desserts.length)];
+    const display = document.getElementById('randomDessert');
+    document.getElementById('randomName').textContent = random.name;
+    document.getElementById('randomImage').innerHTML = 
+    `<img src="${random.image}" alt="${random.name}" 
+    style="width: 150px; height: 150px; object-fit: cover; border-radius: 10px;">`;
+    document.getElementById('randomDesc').textContent = random.description; 
+    display.classList.add('show');
+}
+
 let currentSlide = 0;
 const slides = document.getElementsByClassName("slide");
 
